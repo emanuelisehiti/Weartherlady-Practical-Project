99dc17e775ee2e596f6c2bacfc524f500bef352f
Emanuel <67505364+emanuelisehiti@users.noreply.github.com>
2021-10-10 15:18:46 +0200

    Update UrlConstants.java

--- src/main/java/constants/UrlConstants.java
+++ src/main/java/constants/UrlConstants.java
@@ -3,7 +3,7 @@ package constants;
 public class UrlConstants {
 
     public static final String OpenWeatherUrl = "http://api.openweathermap.org//data//2.5//weather";
-    public static final String OpenWeatherApiKey = "7edda0bea071419a1e1da9c91ac2d4c6";
+    public static final String OpenWeatherApiKey = "";
 
 
 }
